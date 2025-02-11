6f38c4f515272fe678ca86fe4b8d9a7104c6e8ba
Precious <preshengr@gmail.com>
2025-02-11 16:24:38 +0100

    Updated the content of the repo added some more functionality added some error pages removed unwanted pages and files.

--- admin/index.php
+++ admin/index.php
@@ -1,153 +1,88 @@
+<?php require_once('../config.php'); ?>
+ <!DOCTYPE html>
+<html lang="en" class="" style="height: auto;">
+<?php require_once('inc/header.php') ?>
+  <body class="sidebar-mini layout-fixed control-sidebar-slide-open layout-navbar-fixed dark-mode sidebar-mini-md sidebar-mini-xs" data-new-gr-c-s-check-loaded="14.991.0" data-gr-ext-installed="" style="height: auto;">
+    <div class="wrapper">
+     <?php require_once('inc/topBarNav.php') ?>
+     <?php require_once('inc/navigation.php') ?>
+              
+     <?php $page = isset($_GET['page']) ? $_GET['page'] : 'home';  ?>
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper bg-dark pt-3" style="min-height: 567.854px;">
+     
+        <!-- Main content -->
+        <section class="content  text-dark">
+          <div class="container-fluid">
+            <?php 
+              if(!file_exists($page.".php") && !is_dir($page)){
+                  include '404.html';
+              }else{
+                if(is_dir($page))
+                  include $page.'/index.php';
+                else
+                  include $page.'.php';
 
-    <?php
-session_start();
-include("../db.php");
-
-include "sidenav.php";
-include "topheader.php";
-?>
-      <!-- End Navbar -->
-      <div class="content">
-        <div class="container-fluid">
-         <div class="panel-body">
-		<a>
-            <?php  //success message
-            if(isset($_POST['success'])) {
-            $success = $_POST["success"];
-            echo "<h1 style='color:#0C0'>Your Product was added successfully &nbsp;&nbsp;  <span class='glyphicon glyphicon-ok'></h1></span>";
-            }
-            ?></a>
-                </div>
-                <div class="col-md-14">
-            <div class="card ">
-              <div class="card-header card-header-primary">
-                <h4 class="card-title"> Users List</h4>
-              </div>
-              <div class="card-body">
-                <div class="table-responsive ps">
-                  <table class="table table-hover tablesorter " id="">
-                    <thead class=" text-primary">
-                        <tr><th>ID</th><th>FirstName</th><th>LastName</th><th>Email</th><th>Password</th><th>Contact</th><th>Address</th><th>City</th>
-                    </tr></thead>
-                    <tbody>
-                      <?php 
-                        $result=mysqli_query($con,"select * from user_info")or die ("query 1 incorrect.....");
-
-                        while(list($user_id,$first_name,$last_name,$email,$password,$phone,$address1,$address2)=mysqli_fetch_array($result))
-                        {	
-                        echo "<tr><td>$user_id</td><td>$first_name</td><td>$last_name</td><td>$email</td><td>$password</td><td>$phone</td><td>$address1</td><td>$address2</td>
-
-                        </tr>";
-                        }
-                        ?>
-                    </tbody>
-                  </table>
-                <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div></div></div>
-              </div>
-            </div>
+              }
+            ?>
           </div>
-           <div class="row">
-            <div class="col-md-6">
-            <div class="card ">
-              <div class="card-header card-header-primary">
-                <h4 class="card-title"> Categories List</h4>
-              </div>
-              <div class="card-body">
-                <div class="table-responsive ps">
-                  <table class="table table-hover tablesorter " id="">
-                    <thead class=" text-primary">
-                        <tr><th>ID</th><th>Categories</th><th>Count</th>
-                    </tr></thead>
-                    <tbody>
-                      <?php 
-                        $result=mysqli_query($con,"select * from categories")or die ("query 1 incorrect.....");
-                        $i=1;
-                        while(list($cat_id,$cat_title)=mysqli_fetch_array($result))
-                        {	
-                            $sql = "SELECT COUNT(*) AS count_items FROM products WHERE product_cat=$i";
-                            $query = mysqli_query($con,$sql);
-                            $row = mysqli_fetch_array($query);
-                            $count=$row["count_items"];
-                            $i++;
-                        echo "<tr><td>$cat_id</td><td>$cat_title</td><td>$count</td>
-
-                        </tr>";
-                        }
-                        ?>
-                    </tbody>
-                  </table>
-                <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div></div></div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="card ">
-              <div class="card-header card-header-primary">
-                <h4 class="card-title">Brands List</h4>
-              </div>
-              <div class="card-body">
-                <div class="table-responsive ps">
-                  <table class="table table-hover tablesorter " id="">
-                    <thead class=" text-primary">
-                        <tr><th>ID</th><th>Brands</th><th>Count</th>
-                    </tr></thead>
-                    <tbody>
-                      <?php 
-                        $result=mysqli_query($con,"select * from brands")or die ("query 1 incorrect.....");
-                        $i=1;
-                        while(list($brand_id,$brand_title)=mysqli_fetch_array($result))
-                        {	
-                            
-                            $sql = "SELECT COUNT(*) AS count_items FROM products WHERE product_brand=$i";
-                            $query = mysqli_query($con,$sql);
-                            $row = mysqli_fetch_array($query);
-                            $count=$row["count_items"];
-                            $i++;
-                        echo "<tr><td>$brand_id</td><td>$brand_title</td><td>$count</td>
-
-                        </tr>";
-                        }
-                        ?>
-                    </tbody>
-                  </table>
-                <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div></div></div>
-              </div>
-            </div>
-          </div>
-           </div>
-           <div class="col-md-5">
-            <div class="card ">
-              <div class="card-header card-header-primary">
-                <h4 class="card-title">Subscribers</h4>
-              </div>
-              <div class="card-body">
-                <div class="table-responsive ps">
-                  <table class="table table-hover tablesorter " id="">
-                    <thead class=" text-primary">
-                        <tr><th>ID</th><th>email</th>
-                    </tr></thead>
-                    <tbody>
-                      <?php 
-                        $result=mysqli_query($con,"select * from email_info")or die ("query 1 incorrect.....");
-
-                        while(list($brand_id,$brand_title)=mysqli_fetch_array($result))
-                        {	
-                        echo "<tr><td>$brand_id</td><td>$brand_title</td>
-
-                        </tr>";
-                        }
-                        ?>
-                    </tbody>
-                  </table>
-                <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div></div></div>
-              </div>
-            </div>
-          </div>
-           
-            
-          
-        </div>
+        </section>
+        <!-- /.content -->
+  <div class="modal fade" id="confirm_modal" role='dialog'>
+    <div class="modal-dialog modal-md modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title">Confirmation</h5>
       </div>
-      <?php
-include "footer.php";
-?>
+      <div class="modal-body">
+        <div id="delete_content"></div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" id='confirm' onclick="">Continue</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="modal fade" id="uni_modal" role='dialog'>
+    <div class="modal-dialog modal-md modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title"></h5>
+      </div>
+      <div class="modal-body">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" id='submit' onclick="$('#uni_modal form').submit()">Save</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="modal fade" id="uni_modal_right" role='dialog'>
+    <div class="modal-dialog modal-full-height  modal-md" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title"></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span class="fa fa-arrow-right"></span>
+        </button>
+      </div>
+      <div class="modal-body">
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="modal fade" id="viewer_modal" role='dialog'>
+    <div class="modal-dialog modal-md" role="document">
+      <div class="modal-content">
+              <button type="button" class="btn-close" data-dismiss="modal"><span class="fa fa-times"></span></button>
+              <img src="" alt="">
+      </div>
+    </div>
+  </div>
+      </div>
+      <!-- /.content-wrapper -->
+      <?php require_once('inc/footer.php') ?>
+  </body>
+</html>
